c1d9e5ce2fdc6ea7ee50c006fc959d1e8f7ca58a
Ildar Unaybekov <i.unaybekov@zenit.ru>
2019-09-30 16:59:39 +0300

    333

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('boys').onclick = function() {
+    document.getElementById('mega-menu-boys').classList.toggle('mega-menu-display');
+}
+
+document.getElementById('girls').onclick = function() {
+    document.getElementById('mega-menu-girls').classList.toggle('mega-menu-display');
+}
+
+document.getElementById('about').onclick = function() {
+    document.getElementById('mega-menu-about').classList.toggle('mega-menu-display');
+}
